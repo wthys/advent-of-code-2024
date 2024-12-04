ab1c486ff2dcd29701c9d201d14887d9b13dd52f
Robbe Thys <740392+wthys@users.noreply.github.com>
2024-12-04 11:29:56 +0000

    day 4 part 2 + refactoring of word search

--- src/solutions/day4/solution.go
+++ src/solutions/day4/solution.go
@@ -40,8 +40,33 @@ func (s solution) Part1(input []string) (string, error) {
 	return solver.Solved(count)
 }
 
-func (s solution) Part2(input []string) (string, error) {
-	return solver.NotImplemented()
+func (s solution) Part2(input []string) (string, error) {grid, err := parseInput(input)
+	if err != nil {
+		return solver.Error(err)
+	}
+
+	count := 0
+
+	forwardDirs := []L.Location{{1,1},{-1,-1}}
+	backDirs := []L.Location{{-1,1},{1,-1}}
+	grid.ForEach(func (start L.Location, letter rune) {
+		if letter != 'A' {
+			return
+		}
+
+		for _, fdir := range forwardDirs {
+			if !matchMas(grid, start.Add(fdir.Scale(-1)), fdir) {
+				continue
+			}
+			for _, bdir := range backDirs {
+				if matchMas(grid, start.Add(bdir.Scale(-1)), bdir) {
+					count += 1
+				}
+			}
+		}
+	})
+
+	return solver.Solved(count)
 }
 
 func parseInput(input []string) (*G.Grid[rune], error) {
@@ -60,27 +85,19 @@ func parseInput(input []string) (*G.Grid[rune], error) {
 }
 
 func matchXmas(grid *G.Grid[rune], start, direction L.Location) bool {
-	l := '.'
-	ok := fmt.Errorf("nothing really")
+	return matchWord(grid, "XMAS", start, direction)
+}
 
-	l, ok = grid.Get(start)
-	if ok != nil || l != 'X' {
-		return false
-	}
+func matchMas(grid *G.Grid[rune], start, direction L.Location) bool {
+	return matchWord(grid, "MAS", start, direction)
+}
 
-	l, ok = grid.Get(start.Add(direction))
-	if ok != nil || l != 'M' {
-		return false
-	}
-
-	l, ok = grid.Get(start.Add(direction.Scale(2)))
-	if ok != nil || l != 'A' {
-		return false
-	}
-
-	l, ok = grid.Get(start.Add(direction.Scale(3)))
-	if ok != nil || l != 'S' {
-		return false
+func matchWord(grid *G.Grid[rune], word string, start, direction L.Location) bool {
+	for n, letter := range word {
+		l, ok := grid.Get(start.Add(direction.Scale(n)))
+		if ok != nil || l != letter {
+			return false
+		}
 	}
 
 	return true
